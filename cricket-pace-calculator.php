ed7fcc853e193a3ed03becb3c1ab363bff0bdc40
BODAPATI LAKSHMI KANTH VAMSI <blkvamsi9@gmail.com>
2026-01-12 14:20:27 +0530

    Create cricket-pace-calculator.php

---
+++ cricket-pace-calculator.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Cricket Pace Calculator
+ * Plugin URI:  https://github.com/Vamsi0702/php-wordpress-learning
+ * Description: A custom plugin to calculate cricket bowling speeds based on distance and time. Built to demonstrate PHP & JS skills.
+ * Version:     1.0.0
+ * Author:      Vamsi Bodapati
+ * Author URI:  https://github.com/Vamsi0702
+ * License:     GPL-2.0+
+ * Text Domain: cricket-pace-calc
+ */
+
+// Prevent direct access to this file (Security Best Practice)
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; 
+}
+
+// -------------------------------------------------------------------------
+// 🚀 COMING SOON: Logic to handle shortcode [cricket_pace_calc]
+// -------------------------------------------------------------------------
+?>
